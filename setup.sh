c75fe2960d41ee008053ec8e8e329573dc3a3955
Hiroaki Ogasawara <hiroga1030@gmail.com>
2018-06-11 08:11:52 +0900

    blockchain-wallet-serviceã setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+sudo apt-get install -q python-pip
 sudo apt-get install -q python-pygame
-pip install -r requirements.txt
 
+sudo apt install -y npm
+sudo npm install n -g
+sudo n stable
+sudo ln -sf /usr/local/bin/node /usr/bin/node
+sudo ln -s /usr/local/bin/npm /usr/bin/npm
+sudo apt purge -y nodejs npm
+
+pip install -r requirements.txt
+npm install -g blockchain-wallet-service --unsafe-perm
