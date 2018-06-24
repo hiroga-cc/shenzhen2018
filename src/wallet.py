1d69ba0b15f27723d47c40e1d2a285c36881f30b
Hiroaki Ogasawara <hiroga1030@gmail.com>
2018-06-24 17:41:18 +0900

    Add wallet client

---
+++ src/wallet.py
@@ -0,0 +1,30 @@
+import os
+import requests
+
+
+class Wallet:
+    def __init__(self):
+        self.guid = os.environ["GUID"]
+        self.password  = os.environ["WALLET_PASSWORD"]
+        self.api_code = os.environ["WALLET_API_CODE"]
+        self.base_url = "http://localhost:3000"
+
+    def payment(self, to, amount):
+        payment_url = self.base_url + "/merchant/" + self.guid + "/payment?to=" + to + "&amount=" + amount + "&password=" + self.password + "&api_code=" + self.api_code
+        print(payment_url)
+
+        r = requests.post(payment_url)
+        print(r)
+
+    def balance(self):
+        balance_url = self.base_url+ '/merchant/' + self.guid + '/balance?password=' + self.password + "&api_code=" + self.api_code
+        print(balance_url)
+
+        r = requests.post(balance_url)
+        print(r.text)
+
+
+if __name__ == "__main__":
+    wallet = Wallet()
+    wallet.balance()
+
---
+++ start.sh
@@ -0,0 +1,12 @@
+# Set environment variables
+. ./env
+
+# Run bitcoin wallet
+blockchain-wallet-service start --port 3000 &
+
+# Run Garbage Recognition
+
+
+# Run QR Code Recognition
+
+
