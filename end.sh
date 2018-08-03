7e6b36c7a8d54174cf39c299ff7a829e6ed908ad
Hiroaki Ogasawara <hiroga1030@gmail.com>
2018-08-04 06:33:04 +0900

    kill all

--- end.sh
+++ end.sh
@@ -1,2 +1,2 @@
-ps aux | grep python | grep -v grep | awk '{ print "kill -9", $2 }' | sh
-ps aux | grep node   | grep -v grep | awk '{ print "kill -9", $2 }' | sh
+ps aux | grep python | grep -v grep | awk '{ print "sudo kill -9", $2 }' | sh
+ps aux | grep node   | grep -v grep | awk '{ print "sudo kill -9", $2 }' | sh
